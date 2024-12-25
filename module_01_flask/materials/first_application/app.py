6d12cac9f46cec9098f0b6fb655dfaa09f17d5e2
Natalya Pikaleva <deva032006@yandex.ru>
2024-12-25 15:08:31 +0300

    Выполнила практическую работу

--- module_01_flask/materials/first_application/app.py
+++ module_01_flask/materials/first_application/app.py
@@ -1,8 +1,25 @@
-import datetime
+import random
+from datetime import datetime, timedelta
+import re
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
 count = 0
+cars_list = ['Chevrolet', 'Renault', 'Ford', 'Lada']
+cats_list = ['корниш-рекс', "русская голубая", "шотландская вислоухая", "мейн-кун", "манчкин"]
+
+BASE_DIR = 'C:\\Users\\deva0\\PycharmProjects\\python_advanced\\module_01_flask\\homework'
+BOOK_FILE = os.path.join(BASE_DIR, 'war_and_peace.txt')
+
+text_of_book = ''
+
+with open(BOOK_FILE, 'r', encoding='utf8') as file:
+    for i_line in file:
+        text_of_book += i_line
+
+words = re.findall(r'\w+', text_of_book)
 
 @app.route('/test')
 def test_function():
@@ -18,3 +35,35 @@ def counter():
     global count
     count += 1
     return str(count)
+
+@app.route('/cars')
+def cars():
+    global cars_list
+    return ', '.join(cars_list)
+
+@app.route('/cats')
+def cats():
+    global cats_list
+    random_cat = random.choice(cats_list)
+    return random_cat
+
+@app.route('/get_time/now')
+def get_time_now():
+    current_time = datetime.now()
+    return 'Точное время: {}'.format(current_time)
+
+@app.route('/get_time/future')
+def get_time_future():
+    current_time_after_hour = datetime.now() + timedelta(hours=1)
+    return 'Точное время через час будет: {}'.format(current_time_after_hour)
+
+@app.route('/get_random_word')
+def get_random_word():
+    global words
+    random_word = random.choice(words)
+
+    return 'Случайное слово из книги Война и мир: {}'.format(random_word)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
