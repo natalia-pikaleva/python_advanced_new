f77e36363d64983e24c436e5ca695e408dc051a0
Natalya Pikaleva <deva032006@yandex.ru>
2024-12-25 13:20:59 +0300

    Текст вашего комментария

--- module_01_flask/materials/first_application/app.py
+++ module_01_flask/materials/first_application/app.py
@@ -2,9 +2,19 @@ import datetime
 from flask import Flask
 
 app = Flask(__name__)
-
+count = 0
 
 @app.route('/test')
 def test_function():
     now = datetime.datetime.now().utcnow()
-    return f'Это тестовая страничка, ответ сгенерирован в {now}'
+    return f'Это новая тестовая страничка, ответ сгенерирован в {now}'
+
+@app.route('/hello_world')
+def hello_world():
+    return 'Hello, world!'
+
+@app.route('/counter')
+def counter():
+    global count
+    count += 1
+    return str(count)
