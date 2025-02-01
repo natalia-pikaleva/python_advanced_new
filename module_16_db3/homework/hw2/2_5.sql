89809c09d1cbfa3fe0863626289f5ee1b0df253b
Natalya Pikaleva <deva032006@yandex.ru>
2025-02-01 19:39:51 +0300

    Внесла изменения в задачу 2.5

--- module_16_db3/homework/hw2/2_5.sql
+++ module_16_db3/homework/hw2/2_5.sql
@@ -1,15 +1,4 @@
-SELECT DISTINCT
-    CASE
-        WHEN c1.full_name < c2.full_name THEN c1.full_name
-        ELSE c2.full_name
-        END AS customer1,
-    CASE
-        WHEN c1.full_name < c2.full_name THEN c2.full_name
-        ELSE c1.full_name
-        END AS customer2
-
-    FROM customer c1, customer c2
-
-    WHERE c1.city = c2.city
-        AND c1.customer_id <> c2.customer_id
-        AND c1.manager_id = c2.manager_id;
+SELECT c1.full_name, c2.full_name
+FROM customer c1
+JOIN customer c2 ON c1.city = c2.city AND c1.manager_id = c2.manager_id
+WHERE c1.customer_id <> c2.customer_id;
