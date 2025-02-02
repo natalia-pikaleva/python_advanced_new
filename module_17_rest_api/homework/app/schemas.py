85ad7abb13cffe90dff28435314b56c0c2017e1e
Natalya Pikaleva <deva032006@yandex.ru>
2025-02-02 16:55:26 +0300

    Выполнила практическую работу

--- module_17_rest_api/homework/app/schemas.py
+++ module_17_rest_api/homework/app/schemas.py
@@ -1,12 +1,23 @@
 from marshmallow import Schema, fields, validates, ValidationError, post_load
 
-from models import get_book_by_title, Book
+from models import get_book_by_title, Book, Author
+
+
+class AuthorSchema(Schema):
+    id = fields.Int(dump_only=True)
+    first_name = fields.Str(required=True)
+    last_name = fields.Str(required=True)
+    middle_name = fields.Str(allow_none=True)
+
+    @post_load
+    def create_author(self, data: dict, many=True, partial=True) -> Author:
+        return Author(**data)
 
 
 class BookSchema(Schema):
     id = fields.Int(dump_only=True)
     title = fields.Str(required=True)
-    author = fields.Str(required=True)
+    author = fields.Nested(AuthorSchema, required=True)
 
     @validates('title')
     def validate_title(self, title: str) -> None:
@@ -17,5 +28,5 @@ class BookSchema(Schema):
             )
 
     @post_load
-    def create_book(self, data: dict) -> Book:
+    def create_book(self, data: dict, many=True, partial=True) -> Book:
         return Book(**data)
