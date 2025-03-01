5e341faa300d94f41da0d76d303eddb78c946a36
pikaleva_natalya <deva032006@yandex.ru>
2025-03-01 15:02:53 +0000

    Add new files

--- module_30_ci_linters/materials/linters/code.py
+++ module_30_ci_linters/materials/linters/code.py
@@ -1,41 +1,54 @@
 """Module with some typical mistakes. They aimed to be find by linters."""
+
 import multiprocessing
+import sys
+import tempfile
 from typing import Optional
 
 import flask
-
-import tempfile
-import sys
-
-from third_party import lib15, lib1, lib2, lib3, lib4, lib5, lib6, lib7, lib8, lib9, lib10, lib11, lib12, lib13, lib14
-
+# from third_party import (
+#     lib1,
+#     lib2,
+#     lib3,
+#     lib4,
+#     lib5,
+#     lib6,
+#     lib7,
+#     lib8,
+#     lib9,
+#     lib10,
+#     lib11,
+#     lib12,
+#     lib13,
+#     lib14,
+#     lib15,
+# )
 
 
 class BadClass:
-    value: str = 42
+    value: int = 42
 
-    def get_value(self) -> "BadClass":
+    def get_value(self) -> str:
         return "some_other_value"
 
-    def compute_something(self) -> None:
-        if self.value == 42:
-            return True
-        else:
-            return False
+    def compute_something(self) -> int:
+        return self.value == 42
+
 
     def it_will_fail(self):
         return self.other_value
 
 
-def viking_cafe_order(spam: None, beans: str, eggs: Optional[str] = None)\
-        -> str:
+def viking_cafe_order(spam: None, beans: str, eggs: Optional[str] = None) -> str:
     del beans, eggs
-    return spam + spam + spam
+    return f"{spam} {spam} {spam}"
 
 
 import tempfile
 
 
 def compute_other_thing():
-    try: 1 / 0
-    except: print("oops")
+    try:
+        1 / 0
+    except:
+        print("oops")
